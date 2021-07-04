71732438452d06589598f8b8761c16fc0209a68d
kudo_hiroaki <pikoniconico26@gmail.com>
2021-07-05 00:56:40 +0900

    編集機能追加

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -5,4 +5,27 @@ class HousesController < ApplicationController
   def new
     @house = House.new
   end
+  def create
+    @house = House.new(house_params)
+    if @house.save
+      redirect_to houses_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @house = House.find(params[:id])
+  end
+  def update
+    @house = House.find(params[:id])
+    if @house.update(house_params)
+      redirect_to houses_path, notice: "更新しました"
+    else
+      render :edit
+    end
+  end
+  private
+  def house_params
+    params.require(:house).permit(:building, :money, :address, :years, :other)
+  end
 end
